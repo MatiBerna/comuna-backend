57d9f7f7de27a74efc4314332cc6e807b0362539
Matias Bernardi <matias.bernardi.1@gmail.com>
2024-02-27 15:19:40 -0300

    envs

--- src/shared/db/conn.ts
+++ src/shared/db/conn.ts
@@ -1,8 +1,9 @@
 import mongoose from 'mongoose'
 
 export function dbConect() {
+  const url = process.env.dbURL
   mongoose
-    .connect('mongodb://127.0.0.1:27017/comunadb')
+    .connect(url || 'mongodb://127.0.0.1:27017/comunadb')
     .then(() => console.log('MongoDB connected...'))
     .catch((err) => console.log(err))
 }
